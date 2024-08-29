198aa7761f9782152daf774bcaa671832184f74d
Emili Robles <ejrobles@wisc.edu>
2024-08-28 23:00:41 -0500

    Update AboutMe.jsx

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import uwImg from "../Images/uwpic.JPG"
 import {Col, Row, Container, Card, Button} from 'react-bootstrap';
 import wiscience from "../../Assets/wiscience.png";
